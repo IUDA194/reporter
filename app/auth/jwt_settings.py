52e4d14e29cf30744a69384b59f26b50479f2442
iuda194 <popmsee@gmail.com>
2025-05-14 15:12:11 +0200

    add cors

--- app/auth/jwt_settings.py
+++ app/auth/jwt_settings.py
@@ -3,4 +3,4 @@ import jwt
 
 JWT_SECRET = os.getenv("JWT_SECRET", "your_jwt_secret")
 JWT_ALGORITHM = "HS256"
-BOT_URL = os.getenv("BOT_URL", "https://t.me/YOUR_BOT")
+BOT_URL = os.getenv("BOT_URL", "https://t.me/testikkkkki8_bot")
--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from app.routers import service, sockets, task
 
 app = FastAPI()
+
+cors_origins = os.getenv("CORS_ORIGINS", "")
+origins = [origin.strip() for origin in cors_origins.split(",") if origin.strip()]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(service, prefix="/service")
 app.include_router(sockets, prefix="/ws")
